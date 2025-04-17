7de7c8fbcae48c8def9c634d5e296e0a967af015
LordKymzul <dev@heifereum.com>
2025-04-17 13:30:09 +0800

    test

---
+++ backend/src/modules/istore-fulfillment/index.ts
@@ -0,0 +1,9 @@
+import { ModuleProviderExports } from '@medusajs/framework/types'
+
+const services = []
+
+const providerExport: ModuleProviderExports = {
+    services,
+}
+
+export default providerExport
