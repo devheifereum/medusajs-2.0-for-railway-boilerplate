0f75282a66f9bc7d0a727a59255ea5c9516b411f
LordKymzul <dev@heifereum.com>
2025-04-17 14:06:31 +0800

    :dada:

--- storefront/src/lib/data/orders.ts
+++ storefront/src/lib/data/orders.ts
@@ -5,7 +5,7 @@ import medusaError from "@lib/util/medusa-error"
 import { cache } from "react"
 import { getAuthHeaders } from "./cookies"
 
-export const retrieveOrder = cache(async function (id: string) {
+export const retrieveOrder = async function (id: string) {
   return sdk.store.order
     .retrieve(
       id,
@@ -14,9 +14,9 @@ export const retrieveOrder = cache(async function (id: string) {
     )
     .then(({ order }) => order)
     .catch((err) => medusaError(err))
-})
+}
 
-export const listOrders = cache(async function (
+export const listOrders = async function (
   limit: number = 10,
   offset: number = 0
 ) {
@@ -24,4 +24,4 @@ export const listOrders = cache(async function (
     .list({ limit, offset }, { next: { tags: ["order"] }, ...getAuthHeaders() })
     .then(({ orders }) => orders)
     .catch((err) => medusaError(err))
-})
+}
--- storefront/src/lib/data/payment.ts
+++ storefront/src/lib/data/payment.ts
@@ -2,7 +2,7 @@ import { sdk } from "@lib/config"
 import { cache } from "react"
 
 // Shipping actions
-export const listCartPaymentMethods = cache(async function (regionId: string) {
+export const listCartPaymentMethods = async function (regionId: string) {
   return sdk.store.payment
     .listPaymentProviders(
       { region_id: regionId },
@@ -12,4 +12,4 @@ export const listCartPaymentMethods = cache(async function (regionId: string) {
     .catch(() => {
       return null
     })
-})
+}
